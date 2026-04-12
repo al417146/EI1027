835092b38e4dcfc2bbc757da85b067ccbe5623b7
Pau <al417146@uji.es>
2026-04-12 23:35:51 +0200

    fe

--- src/main/java/es/uji/ei1027/proyecto/controlador/OVIUserController.java
+++ src/main/java/es/uji/ei1027/proyecto/controlador/OVIUserController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
+//CAMBIOSSSSSSSSSSSSSSSSSSSS
 import java.security.Principal;
 
 @Controller
